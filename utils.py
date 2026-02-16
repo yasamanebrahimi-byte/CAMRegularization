d5bea8370442584079d14536781aace68542dfd7
Yasaman Ebrahimi <yasaman.ebrahimi@sjsu.edu>
2026-02-16 10:40:10 -0800

    made reproducibility stronger

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import os
 import random
 import torch
-
+import torch.backends.cudnn as cudnn
 
 def set_seed(seed):
     random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-
+    cudnn.deterministic = True
+    cudnn.benchmark = False
 
 @torch.no_grad()
 def accuracy_top1(logits, targets):
